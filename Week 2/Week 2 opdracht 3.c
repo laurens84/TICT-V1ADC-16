a5006a110fe32dbdc5b3ef7b71c4ea4afae1c5e4
laurens84 <laurens.vandersluis@student.hu.nl>
2017-02-19 19:06:32 +0100

    Added handling for strings longer than 80 characters.

--- Week 2/Week 2 opdracht 3.c
+++ Week 2/Week 2 opdracht 3.c
@@ -8,13 +8,18 @@
 #include <stdio.h>
 #include <string.h>
 
-#define size 80
+#define size 82
 
 int main(int argc, char **argv){
     char input[size] = {};
     
     printf("Enter text to display backwards.\n");
     fgets(input, size, stdin);
+    if ((input[80] != 10) && (input[80] != 0)){
+        printf("Warning! The string you gave is longer than 80 characters! Cutting off string!\n");
+        input[80] = 10;
+        while (getchar() != '\n');
+    }
     for (int i = (strlen(input) - 1); i >= 0; i--){
         
         putchar(input[i]);
