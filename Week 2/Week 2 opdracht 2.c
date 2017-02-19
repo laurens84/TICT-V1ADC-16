38af6da21e45496305d4b574e890455c770f12f4
laurens84 <laurens.vandersluis@student.hu.nl>
2017-02-19 18:51:44 +0100

    Update Week 2 opdracht 2.c
    The application now won't fail when one of the input strings is more than 80 characters long.

--- Week 2/Week 2 opdracht 2.c
+++ Week 2/Week 2 opdracht 2.c
@@ -7,7 +7,7 @@
 
 #include <stdio.h>
 
-#define size 80
+#define size 82
 
 int main(int argc, char **argv)
 {
@@ -17,14 +17,25 @@ int main(int argc, char **argv)
     
     printf("Enter the first string: ");
     fgets(string1, size, stdin);
+    printf("appels %d", string1[81]);
+    if ((string1[80] != 10) && (string1[80] != 0)){
+        printf("Warning! The string you gave is longer than 80 characters. Cutting off string!");
+        string1[80] = 10;
+        while (getchar() != '\n');
+    }
     printf("Enter the second string: ");
     fgets(string2, size, stdin);
+    if ((string2[80] != 10) && (string2[80] != 0)){
+        printf("Warning! The string you gave is longer than 80 characters! Cutting off string!");
+        string2[80] = 10;
+        while (getchar() != 10);
+    }
     while ((string1[index] == string2[index]) && (index < size)){
         index++;
         if (index >= size) printf("Those strings are exactly the same.");
     }
     if (index < size){
-        printf("Those two strings differ after the %dth character.\n", (index + 1));
+        printf("Those two strings start to differ at character number %d.\n", (index + 1));
         printf("String 1: %s", string1);
         printf("String 2: %s%*c\n", string2, (index + 11), '^');
     }
